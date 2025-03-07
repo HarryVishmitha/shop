ac60fd1ae488b271fc1a6e91a5dd0ce70ab48758
HarryVishmitha <vishmithathejan154@gmail.com>
2025-03-07 16:44:45 +0530

    by 2119

--- resources/js/Pages/admin/dashboard.jsx
+++ resources/js/Pages/admin/dashboard.jsx
@@ -6,6 +6,7 @@ import CookiesV from '@/Components/CookieConsent';
 import { Icon } from "@iconify/react";
 import AdminDashboard from '../../Layouts/AdminDashboard';
 import Breadcrumb from "@/components/Breadcrumb";
+// import Metrics from '@/Components/Metrics';
 
 const Dashboard = ({ totalUsers, adminUsers, userUsers, workingGroups, dailyCustomers}) => {
     const breadcrumbs = [
@@ -17,7 +18,35 @@ const Dashboard = ({ totalUsers, adminUsers, userUsers, workingGroups, dailyCust
             <Head title="Admin Dashboard" />
             <AdminDashboard>
                 <Breadcrumb title="Panels" />
-                hi
+                {/* adding Metrics */}
+                <div className="row row-cols-xxxl-5 row-cols-lg-3 row-cols-sm-2 row-cols-1 gy-4">
+
+                    {/* Users count */}
+                    <div className="col">
+                        <div className="card shadow-none border bg-gradient-start-1">
+                            <div className="card-body p-20">
+                                <div className="d-flex flex-wrap align-items-center justify-content-between gap-3">
+                                    <div>
+                                        <p className="fw-medium text-primary-light mb-1">
+                                            Total Users
+                                        </p>
+                                        <h6 className="mb-0">{totalUsers}</h6>
+                                    </div>
+                                    <div className="w-50-px h-50-px bg-cyan rounded-circle d-flex justify-content-center align-items-center">
+                                        <Icon
+                                            icon="gridicons:multiple-users"
+                                            className="text-base text-2xl mb-0"
+                                        />
+                                    </div>
+                                </div>
+                                <p>
+                                    hi
+                                </p>
+                            </div>
+                        </div>
+                        {/* card end */}
+                    </div>
+                </div>
             </AdminDashboard>
             <CookiesV />
         </>
